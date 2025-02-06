dbf8c6eb1f44f1a5d882251406abbbfb049d6f66
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-02-06 21:11:55 +0530

    feat: add website skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 function App() {
-  return <button className="btn btn-primary">Hello</button>;
+  return (
+    <div>
+      <div className="row">
+        <div className="col-md-12 bg-primary">Hello</div>
+      </div>
+      <div className="row">
+        <div className="col-sm-12 col-md-3 d-none d-md-block bg-danger">
+          BOI
+        </div>
+        <div className="col-sm-12 col-md-9 bg-success">Bye</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
