6a7a26ead5dca935f5c6be6ff490aa3f93dfd96b
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-02-09 22:48:32 +0530

    feat: add new card and fix CSS styles

--- src/components/main/Body.tsx
+++ src/components/main/Body.tsx
@@ -1,4 +1,5 @@
 import Spiderman from "../../assets/GameIcons/Spiderman-Rem.jpg";
+import DaysGone from "../../assets/GameIcons/DaysGone.jpg";
 import Like from "./utils/Like";
 
 const Body = () => {
@@ -17,7 +18,12 @@ const Body = () => {
         <div className="row gap-3">
           <div className="col-lg-3 col-md-5 col-sm-5 bg-dark bg-gradient row gap-2 m-2 p-0 rounded-3">
             <div className="row p-0 m-0 ">
-              <img src={Spiderman} alt="" className="p-0 rounded float-start" />
+              <img
+                src={Spiderman}
+                alt=""
+                className="p-0 rounded float-start"
+                style={{ width: "100%", height: "300px", objectFit: "cover" }}
+              />
             </div>
             <div className="row gap-3 p-2 m-0 text-light">
               <div className="d-flex align-items-center gap-2">Studios</div>
@@ -34,14 +40,17 @@ const Body = () => {
           </div>
           <div className="col-lg-3 col-md-5 col-sm-5 bg-dark bg-gradient row gap-2 m-2 p-0 rounded-3">
             <div className="row p-0 m-0 ">
-              <img src={Spiderman} alt="" className="p-0 rounded float-start" />
+              <img
+                src={DaysGone}
+                alt=""
+                className="p-0 rounded float-start"
+                style={{ width: "100%", height: "300px", objectFit: "cover" }}
+              />
             </div>
             <div className="row gap-3 p-2 m-0 text-light">
               <div className="d-flex align-items-center gap-2">Studios</div>
               <div className="d-flex align-items-center gap-2">
-                <h1 className="fs-3 fw-medium m-0 p-0">
-                  Spider-man Remastered
-                </h1>
+                <h1 className="fs-3 fw-medium m-0 p-0">Days Gone</h1>
               </div>
               <div className="d-flex align-items-center gap-2">
                 <Like />
