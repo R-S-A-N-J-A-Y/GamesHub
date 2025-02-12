aa824d6117f6af98800c8da23e4ad05243409472
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-02-12 22:36:29 +0530

    style: make sidebar fixed and add hover effect

--- src/components/Layout/sidebar/SideBar.tsx
+++ src/components/Layout/sidebar/SideBar.tsx
@@ -1,45 +1,62 @@
 import getGenres from "../../../hooks/getGenres";
+import styled from "styled-components";
+
+const NavbarItem = styled.a`
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background: rgba(148, 145, 145, 0.3);
+  }
+`;
 
 const SideBar = () => {
   const genres = getGenres();
 
   return (
     <div
-      className="col-lg-2 d-none d-lg-block border border-start-0 bg-black bg-opacity-10 border-dark-subtle rounded-end p-0 position-fixed"
-      style={{ background: "whitesmoke" }}
+      className="col-lg-2 d-none d-lg-block rounded-end-4 p-0 position-fixed"
+      style={{ background: "white" }}
     >
-      <nav className="navbar navbar-expand-lg m-2 p-2">
+      <nav className="navbar navbar-expand-lg m-2 p-1">
         <div className="navbar-fluid">
-          <ul className="navbar-nav nav">
-            <li className="navbar-item">
-              <a href="#" className="nav-link fs-4 fw-bolder m-0">
+          <ul className="navbar-nav nav d-flex gap-2">
+            <li className="navbar-item w-100">
+              <NavbarItem
+                href="#"
+                className="nav-link fs-4 fw-bolder ms-2 rounded-3"
+              >
                 Reviews
-              </a>
+              </NavbarItem>
             </li>
-            <li className="navbar-item">
+            <li className="navbar-item w-100 border rounded-3 px-2 py-1">
               <h1 className="nav-link fs-4 fw-bolder m-0" role="button">
                 New Releases
               </h1>
-              <a href="#" className="nav-link" role="button">
+              <NavbarItem href="#" className="nav-link rounded-3" role="button">
                 Last 30 Days
-              </a>
-              <a href="#" className="nav-link" role="button">
+              </NavbarItem>
+              <NavbarItem href="#" className="nav-link rounded-3" role="button">
                 This Week
-              </a>
-              <a href="#" className="nav-link" role="button">
+              </NavbarItem>
+              <NavbarItem href="#" className="nav-link rounded-3" role="button">
                 Next Week
-              </a>
+              </NavbarItem>
             </li>
-            <li className="navbar-item">
+            <li className="navbar-item w-100 border rounded-3 px-2 py-1">
               <h1 className="nav-link fs-4 fw-bolder m-0" role="button">
                 Generes
               </h1>
               {genres.length > 0 ? (
                 genres.map((g, index) => (
-                  <a key={index} href="#" className="nav-link" role="button">
+                  <NavbarItem
+                    key={index}
+                    href="#"
+                    className="nav-link rounded-3"
+                    role="button"
+                  >
                     {" "}
                     {g.charAt(0).toUpperCase() + g.slice(1)}
-                  </a>
+                  </NavbarItem>
                 ))
               ) : (
                 <p> Error Loading the Genres... </p>
