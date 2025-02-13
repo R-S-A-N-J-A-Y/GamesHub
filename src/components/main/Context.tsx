c86f8bcdd1d3346f5ef9061dfae3dea4a39f69eb
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-02-13 20:20:50 +0530

    feat: implement responsive grid layout for content

--- src/components/main/Context.tsx
+++ src/components/main/Context.tsx
@@ -39,13 +39,13 @@ const Context = () => {
   return (
     <div
       ref={containeRef}
-      className="container-sm-100 p-2 d-flex flex-column gap-3 p-4"
+      className="container-fluid d-flex flex-column gap-3 p-0 border w-75"
       style={{
-        marginTop: "100px",
-        marginLeft: marginL,
+        margin: "100px 0 0 300px",
+        // marginLeft: marginL,
       }}
     >
-      <div className="container p-0 m-0">
+      <div className="container-fluid p-0 m-0">
         <h1 className="fw-bolder" style={{ fontSize: "50px" }}>
           Hot and Trending
         </h1>
@@ -54,53 +54,52 @@ const Context = () => {
         </h1>
       </div>
 
-      <div
-        className="container p-0 m-0 d-flex flex-wrap gap-5"
-        style={{ justifyContent: itemRow === 1 ? "center" : "start" }}
-      >
-        {data.length > 0 ? (
-          data.map((game, index) => (
-            <div
-              key={index}
-              className="row container p-2 m-0 h-100"
-              style={{ minWidth: "280px", maxWidth: "300px" }}
-            >
-              <div className="container p-0" style={{ height: "250px" }}>
-                <img
-                  src={game.imageSrc}
-                  alt={game.name}
-                  style={{ width: "100%", height: "250px", objectFit: "cover" }}
-                  className="rounded-top-4"
-                />
-              </div>
-              <div
-                className="container p-3 d-flex flex-column justify-content-between bg-dark bg-gradient text-light rounded-bottom-4"
-                style={{ gap: "15px" }}
-              >
-                <div
-                  className="container p-0 d-flex justify-content-start"
-                  style={{ gap: "10px" }}
-                >
-                  {game.platforms.includes("ps5") && <FaPlaystation />}
-                  {game.platforms.includes("xbox") && <FaXbox />}
-                  {game.platforms.includes("pc") && <FaWindows />}
+      <div className="container-fluid p-0 m-0">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-5 row-cols-xxxl-6">
+          {data.length > 0 ? (
+            data.map((game, index) => (
+              <div key={index} className="col mb-5">
+                <div className="container p-0">
+                  <img
+                    src={game.imageSrc}
+                    alt={game.name}
+                    style={{
+                      width: "100%",
+                      height: "250px",
+                      objectFit: "cover",
+                    }}
+                    className="rounded-top-4"
+                  />
                 </div>
                 <div
-                  className="container p-0 fw-medium"
-                  style={{ fontSize: "24px", lineHeight: "1.2" }}
+                  className="container p-3 d-flex flex-column justify-content-between bg-dark bg-gradient text-light rounded-bottom-4"
+                  style={{ gap: "15px" }}
                 >
-                  {game.name}
-                </div>
-                <div className="container p-0 d-flex justify-content-start">
-                  <Like />
-                  <p className="m-0 mx-2 p-0">{game.likes}</p>
+                  <div
+                    className="container p-0 d-flex justify-content-start"
+                    style={{ gap: "10px" }}
+                  >
+                    {game.platforms.includes("ps5") && <FaPlaystation />}
+                    {game.platforms.includes("xbox") && <FaXbox />}
+                    {game.platforms.includes("pc") && <FaWindows />}
+                  </div>
+                  <div
+                    className="container p-0 fw-medium"
+                    style={{ fontSize: "24px", lineHeight: "1.2" }}
+                  >
+                    {game.name}
+                  </div>
+                  <div className="container p-0 d-flex justify-content-start">
+                    <Like />
+                    <p className="m-0 mx-2 p-0">{game.likes}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))
-        ) : (
-          <p>Loading games...</p>
-        )}
+            ))
+          ) : (
+            <p>Loading games...</p>
+          )}
+        </div>
       </div>
     </div>
   );
