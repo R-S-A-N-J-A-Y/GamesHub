83433b4e6c2a0197f12cb706c79e15472047f5fc
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-02-23 20:47:23 +0530

    feat: create user object in Sign In page

--- src/components/auth/SignIn.tsx
+++ src/components/auth/SignIn.tsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
 import { Props } from "../../App";
 
 const SignIn = ({ onClick }: Props) => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const OnSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    console.log(user);
+  };
+
   return (
     <>
       <button
@@ -9,25 +21,42 @@ const SignIn = ({ onClick }: Props) => {
       >
         Home
       </button>
+
       <form className="col p-3 bg-dark-subtle m-5 p-5">
         <div className="mb-3">
           <label htmlFor="" className="form-label">
             Name
           </label>
-          <input type="text" className="form-control" />
+          <input
+            type="text"
+            className="form-control"
+            onChange={(e) => setUser({ ...user, name: e.target.value })}
+          />
         </div>
         <div className="mb-3">
           <label htmlFor="" className="form-label">
             Email
           </label>
-          <input type="email" className="form-control" />
+          <input
+            type="email"
+            className="form-control"
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          />
         </div>
         <div className="mb-3">
           <label htmlFor="" className="form-label">
             Password
           </label>
-          <input type="password" className="form-control" />
+          <input
+            type="password"
+            className="form-control"
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+          />
         </div>
+
+        <button className="btn btn-primary p-2" onClick={OnSubmit}>
+          Sign IN
+        </button>
       </form>
     </>
   );
