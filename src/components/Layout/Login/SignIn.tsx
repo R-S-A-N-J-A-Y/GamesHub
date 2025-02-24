36233a0bbeab770a90d9e367c260a2abd78a1a7d
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-02-24 21:31:29 +0530

    feat: complete SignIn.tsx with user data handling

--- src/components/Layout/Login/SignIn.tsx
+++ src/components/Layout/Login/SignIn.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import { useState } from "react";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleLogin = () => {
+    console.log(user);
+  };
+
+  return (
+    <>
+      <div className="m-5 p-3 d-flex gap-3">
+        <div className="flex-grow-1">hi</div>
+        <div className="p-3 flex-grow-1 d-flex flex-column gap-2 bg-dark">
+          <div className="form-floating mb-3">
+            <input
+              type="email"
+              className="form-control"
+              id="floatingInput"
+              placeholder="name@example.com"
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
+            />
+            <label>Email address</label>
+          </div>
+          <div className="form-floating">
+            <input
+              type="password"
+              className="form-control"
+              id="floatingPassword"
+              placeholder="Password"
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+            />
+            <label>Password</label>
+          </div>
+          <button
+            className="mt-3 btn btn-primary align-self-start"
+            onClick={handleLogin}
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SignIn;
