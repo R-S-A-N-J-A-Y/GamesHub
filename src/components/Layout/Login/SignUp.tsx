a466cb11a9f52fb207d37f869dddff19c9546304
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-02-28 20:52:14 +0530

    feat: add form validation for User Sign Up

--- src/components/Layout/Login/SignUp.tsx
+++ src/components/Layout/Login/SignUp.tsx
@@ -1,19 +1,37 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { IoEyeOffOutline } from "react-icons/io5";
 import { AiOutlineEye } from "react-icons/ai";
 
-interface FormData {
-  name: string;
-  email: string;
-  password: string;
-}
+const schema = z
+  .object({
+    name: z
+      .string()
+      .min(3, { message: "Name must be atleast 3  Characters Long." }),
+    email: z.string().email({ message: "Invalid Email Address." }),
+    password: z
+      .string()
+      .min(6, { message: "Password must be at least of 6 Characters Long." }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match.",
+    path: ["confirmPassword"],
+  });
+
+type FormData = z.infer<typeof schema>;
 
 const SignUp = () => {
-  const { register, handleSubmit } = useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
   const [showP, setShowP] = useState(false);
-  const [showCP, setShowCP] = useState(false);
 
   const onSubmit = (user: FormData) => {
     console.log(user);
@@ -21,67 +39,102 @@ const SignUp = () => {
 
   return (
     <div className="mx-3 my-5 p-5 d-flex p-3 gap-3">
-      <div className="p-4 bg-dark flex-grow-1 rounded-3">
+      <div className="p-4 bg-dark  flex-grow-1 rounded-3">
         <form
-          className="d-flex flex-column gap-3"
+          className="d-flex flex-column gap-4"
           onSubmit={handleSubmit(onSubmit)}
         >
-          <div className="form-floating mb-3">
+          <div className="form-floating d-flex flex-column gap-3">
             <input
               type="text"
-              className="form-control"
+              className={`form-control ${errors.name ? "is-invalid" : ""}`}
               placeholder="Username"
               {...register("name")}
             />
             <label> Name </label>
+            {errors.name && (
+              <p className="m-0 p-0 text-danger fw-bold">
+                {" "}
+                {errors.name.message}{" "}
+              </p>
+            )}
           </div>
-          <div className="form-floating mb-3">
+          <div className="form-floating d-flex flex-column gap-2">
             <input
               type="text"
-              className="form-control"
+              className={`form-control ${errors.email ? "is-invalid" : ""}`}
               placeholder="name@example.com"
               {...register("email")}
             />
             <label> Email </label>
+            {errors.email && (
+              <p className="m-0 p-0 text-danger fw-bold">
+                {" "}
+                {errors.email.message}{" "}
+              </p>
+            )}
           </div>
-          <div className="form-floating d-flex align-items-center bg-white rounded-3 justify-content-around mb-3 px-2">
-            <input
-              type={showCP ? "text" : "password"}
-              className="form-control border-0"
-              placeholder="password"
-              {...register("password")}
-            />
-            <button
-              style={{ border: "none", background: "none" }}
-              onClick={(e) => {
-                e.preventDefault();
-                setShowCP(!showCP);
-              }}
-            >
-              {!showCP && <IoEyeOffOutline />}
-              {showCP && <AiOutlineEye />}
-            </button>
-            <label> Password </label>
+          <div className="form-floating d-flex flex-column gap-2">
+            <div className="form-floating d-flex align-items-center bg-white rounded-3 justify-content-around px-2">
+              <input
+                type={showP ? "text" : "password"}
+                className={`form-control border-0 ${
+                  errors.password ? "is-invalid" : ""
+                }`}
+                placeholder="password"
+                {...register("password")}
+              />
+              <button
+                style={{ border: "none", background: "none" }}
+                onClick={(e) => {
+                  e.preventDefault();
+                  setShowP(!showP);
+                }}
+              >
+                {!showP && <IoEyeOffOutline />}
+                {showP && <AiOutlineEye />}
+              </button>
+              <label> Password </label>
+            </div>
+            {errors.password && (
+              <p className="m-0 p-0 text-danger fw-bold">
+                {" "}
+                {errors.password.message}{" "}
+              </p>
+            )}
           </div>
-          <div className="form-floating d-flex align-items-center bg-white rounded-3 justify-content-around mb-3 px-2">
-            <input
-              type={showP ? "text" : "password"}
-              className="form-control border-0"
-              placeholder="CPassword"
-            />
-            <button
-              style={{ border: "none", background: "none" }}
-              onClick={(e) => {
-                e.preventDefault();
-                setShowP(!showP);
-              }}
-            >
-              {!showP && <IoEyeOffOutline />}
-              {showP && <AiOutlineEye />}
-            </button>
-            <label> Confirm Password </label>
+
+          <div className="form-floating d-flex flex-column gap-2">
+            <div className="form-floating d-flex align-items-center bg-white rounded-3 justify-content-around px-2">
+              <input
+                type={showP ? "text" : "password"}
+                className={`form-control border-0 ${
+                  errors.confirmPassword ? "is-invalid" : ""
+                }`}
+                placeholder="CPassword"
+                {...register("confirmPassword")}
+              />
+              <button
+                style={{ border: "none", background: "none" }}
+                onClick={(e) => {
+                  e.preventDefault();
+                  setShowP(!showP);
+                }}
+              >
+                {!showP && <IoEyeOffOutline />}
+                {showP && <AiOutlineEye />}
+              </button>
+              <label> Confirm Password </label>
+            </div>
+            {errors.confirmPassword && (
+              <p className="m-0 p-0 text-danger fw-bold">
+                {" "}
+                {errors.confirmPassword.message}{" "}
+              </p>
+            )}
           </div>
-          <div className="form-floating d-flex align-items-center justify-start gap-3">
+
+          <div className="form-floating d-flex align-items-center justify-start gap-4">
             <button
               className="p-2 btn btn-primary align-self-start fw-bold"
               type="submit"
