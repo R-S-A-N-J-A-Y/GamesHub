bfd60c79bbe8377655a3b9f88bdcd4db58c5de06
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-02-28 19:54:22 +0530

    feat: storing the data entered by user and handled the submit

--- src/components/Layout/Login/SignUp.tsx
+++ src/components/Layout/Login/SignUp.tsx
@@ -1,20 +1,37 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 import { IoEyeOffOutline } from "react-icons/io5";
 import { AiOutlineEye } from "react-icons/ai";
 
+interface FormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
 const SignUp = () => {
+  const { register, handleSubmit } = useForm<FormData>();
   const [showP, setShowP] = useState(false);
   const [showCP, setShowCP] = useState(false);
+
+  const onSubmit = (user: FormData) => {
+    console.log(user);
+  };
+
   return (
     <div className="mx-3 my-5 p-5 d-flex p-3 gap-3">
       <div className="p-4 bg-dark flex-grow-1 rounded-3">
-        <form className="d-flex flex-column gap-3">
+        <form
+          className="d-flex flex-column gap-3"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <div className="form-floating mb-3">
             <input
               type="text"
               className="form-control"
               placeholder="Username"
+              {...register("name")}
             />
             <label> Name </label>
           </div>
@@ -23,6 +40,7 @@ const SignUp = () => {
               type="text"
               className="form-control"
               placeholder="name@example.com"
+              {...register("email")}
             />
             <label> Email </label>
           </div>
@@ -30,7 +48,8 @@ const SignUp = () => {
             <input
               type={showCP ? "text" : "password"}
               className="form-control border-0"
-              placeholder="name@example.com"
+              placeholder="password"
+              {...register("password")}
             />
             <button
               style={{ border: "none", background: "none" }}
@@ -48,7 +67,7 @@ const SignUp = () => {
             <input
               type={showP ? "text" : "password"}
               className="form-control border-0"
-              placeholder="name@example.com"
+              placeholder="CPassword"
             />
             <button
               style={{ border: "none", background: "none" }}
