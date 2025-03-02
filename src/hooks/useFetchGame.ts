e39314c17ea453b5350af87e76ad9ed95b6daf9f
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-03-02 21:38:17 +0530

    feat: fetch game data from database via server

--- src/hooks/useFetchGame.ts
+++ src/hooks/useFetchGame.ts
@@ -3,11 +3,12 @@ import { useEffect, useState } from "react"
 const useFetchGame = () => {
     const [data, setData] = useState<any []>([]);
     useEffect( () => {
-        fetch("/Data/gameCard.json")
+        fetch("http://localhost:3000/getgame")
         .then(res => res.json())
         .then(data => setData(data))
         .catch(err => console.log("Error Loading Genres: ", err.message))
     }, [] );
+    console.log(data);
     return {data};
 }
 
