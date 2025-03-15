20ca29e58048be995abce4200f4a163984732f04
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-03-15 20:59:36 +0530

    Added clear genre selection functionality in Admin sidebar

--- src/components/Layout/App/Admin/utils/SideBar.tsx
+++ src/components/Layout/App/Admin/utils/SideBar.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import getGenres from "../../../../../hooks/getGenres";
 import styled from "styled-components";
+import { IoClose } from "react-icons/io5";
 
-const NavbarItem = styled.a`
+const NavbarAnchor = styled.a`
+  transition: background-color 0.3s ease;
+  text-decoration: none;
+
+  &:hover {
+    background: rgba(148, 145, 145, 0.3);
+  }
+`;
+
+const LiHover = styled.li`
   transition: background-color 0.3s ease;
 
   &:hover {
@@ -15,15 +26,24 @@ interface Props {
 
 const SideBar = ({ onClick }: Props) => {
   const genres = getGenres();
+  const [selectedGenre, setSelectedGenre] = useState<number | null>(null);
 
   const handleGenresClick = (
-    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent> | string
   ) => {
+    console.log(e);
+    if (typeof e === "string") {
+      onClick("clear");
+      setSelectedGenre(null);
+      return;
+    }
+    console.log("i");
     const genreI = e.currentTarget.getAttribute("data-key");
     if (genreI === null) return;
 
     const genreIndex = parseInt(genreI, 10);
     if (isNaN(genreIndex)) return;
+    setSelectedGenre(genreIndex);
     onClick(genres[genreIndex].toLowerCase());
   };
 
@@ -37,47 +57,81 @@ const SideBar = ({ onClick }: Props) => {
     >
       <nav className="navbar navbar-expand-lg m-2 p-1">
         <div className="navbar-fluid">
-          <ul className="navbar-nav nav d-flex gap-2">
+          <ul className="navbar-nav nav d-flex gap-2 list-group">
             <li className="navbar-item w-100">
-              <NavbarItem
+              <NavbarAnchor
                 href="#"
                 className="nav-link fs-4 fw-bolder ms-2 rounded-3"
               >
                 Reviews
-              </NavbarItem>
+              </NavbarAnchor>
             </li>
             <li className="navbar-item w-100 border rounded-3 px-2 py-1">
               <h1 className="nav-link fs-4 fw-bolder m-0" role="button">
                 New Releases
               </h1>
-              <NavbarItem href="#" className="nav-link rounded-3" role="button">
+              <NavbarAnchor
+                href="#"
+                className="nav-link rounded-3"
+                role="button"
+              >
                 Last 30 Days
-              </NavbarItem>
-              <NavbarItem href="#" className="nav-link rounded-3" role="button">
+              </NavbarAnchor>
+              <NavbarAnchor
+                href="#"
+                className="nav-link rounded-3"
+                role="button"
+              >
                 This Week
-              </NavbarItem>
-              <NavbarItem href="#" className="nav-link rounded-3" role="button">
+              </NavbarAnchor>
+              <NavbarAnchor
+                href="#"
+                className="nav-link rounded-3"
+                role="button"
+              >
                 Next Week
-              </NavbarItem>
+              </NavbarAnchor>
             </li>
             <li className="navbar-item w-100 border rounded-3 px-2 py-1">
               <h1 className="nav-link fs-4 fw-bolder m-0" role="button">
                 Generes
               </h1>
               {genres.length > 0 ? (
-                genres.map((g, index) => (
-                  <NavbarItem
-                    key={index}
-                    href="#"
-                    className="nav-link rounded-3"
-                    role="button"
-                    data-key={index}
-                    onClick={handleGenresClick}
-                  >
-                    {" "}
-                    {g.charAt(0).toUpperCase() + g.slice(1)}
-                  </NavbarItem>
-                ))
+                <ul className="list-group">
+                  {genres.map((g, index) => (
+                    <LiHover
+                      key={index}
+                      className={`list-group-item px-2 py-0 border-0 d-flex justify-content-between align-items-center rounded-3  
+                      }`}
+                      style={{
+                        background:
+                          selectedGenre === index
+                            ? "rgba(148, 145, 145, 0.3)"
+                            : "",
+                      }}
+                    >
+                      <a
+                        className={`nav-link ${
+                          selectedGenre === index ? "" : "w-100"
+                        }`}
+                        href="#"
+                        role="button"
+                        data-key={index}
+                        onClick={handleGenresClick}
+                      >
+                        <span>{g.charAt(0).toUpperCase() + g.slice(1)}</span>
+                      </a>
+                      {selectedGenre === index && (
+                        <button
+                          className="btn btn-sm p-0 m-0"
+                          onClick={() => handleGenresClick("clear")}
+                        >
+                          <IoClose color="red" size={20} />
+                        </button>
+                      )}
+                    </LiHover>
+                  ))}
+                </ul>
               ) : (
                 <p> Error Loading the Genres... </p>
               )}
