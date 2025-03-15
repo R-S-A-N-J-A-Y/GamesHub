9a008953df08d998af585df0a0c7bcdf5cfd3313
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-03-15 21:06:43 +0530

    Moved sidebar genre section to a separate page

---
+++ src/components/Layout/App/Admin/utils/SideBarUtils/GenreSection.tsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import getGenres from "../../../../../../hooks/getGenres";
+import styled from "styled-components";
+import { IoClose } from "react-icons/io5";
+
+const LiHover = styled.li`
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background: rgba(148, 145, 145, 0.3);
+  }
+`;
+
+interface Props {
+  onClick: (p: string) => void;
+}
+
+const GenreSection = ({ onClick }: Props) => {
+  const genres = getGenres();
+  const [selectedGenre, setSelectedGenre] = useState<number | null>(null);
+
+  const handleGenresClick = (
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent> | string
+  ) => {
+    console.log(e);
+    if (typeof e === "string") {
+      onClick("clear");
+      setSelectedGenre(null);
+      return;
+    }
+    console.log("i");
+    const genreI = e.currentTarget.getAttribute("data-key");
+    if (genreI === null) return;
+
+    const genreIndex = parseInt(genreI, 10);
+    if (isNaN(genreIndex)) return;
+    setSelectedGenre(genreIndex);
+    onClick(genres[genreIndex].toLowerCase());
+  };
+
+  return (
+    <li className="navbar-item w-100 border rounded-3 px-2 py-1">
+      <h1 className="nav-link fs-4 fw-bolder m-0" role="button">
+        Generes
+      </h1>
+      {genres.length > 0 ? (
+        <ul className="list-group">
+          {genres.map((g, index) => (
+            <LiHover
+              key={index}
+              className={`list-group-item px-2 py-0 border-0 d-flex justify-content-between align-items-center rounded-3  
+                         }`}
+              style={{
+                background:
+                  selectedGenre === index ? "rgba(148, 145, 145, 0.3)" : "",
+              }}
+            >
+              <a
+                className={`nav-link ${selectedGenre === index ? "" : "w-100"}`}
+                href="#"
+                role="button"
+                data-key={index}
+                onClick={handleGenresClick}
+              >
+                <span>{g.charAt(0).toUpperCase() + g.slice(1)}</span>
+              </a>
+              {selectedGenre === index && (
+                <button
+                  className="btn btn-sm p-0 m-0"
+                  onClick={() => handleGenresClick("clear")}
+                >
+                  <IoClose color="red" size={20} />
+                </button>
+              )}
+            </LiHover>
+          ))}
+        </ul>
+      ) : (
+        <p> Error Loading the Genres... </p>
+      )}
+    </li>
+  );
+};
+
+export default GenreSection;
